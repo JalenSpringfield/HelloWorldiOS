f5ceaf3dc7b431458506cc6f03b8ba058a1fa3d0
Jalen Springfield <springfield.jalen@gmail.com>
2018-09-30 23:29:11 -0400

    Completed base proj requirements and updated README

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBAction func didTapButton(_ sender: UIButton) {
+        print("Hello")
+        textLabel.textColor = UIColor.green
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
